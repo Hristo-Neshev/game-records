97905fbd166815dd9b10fdca8ff3b9f25e623a31
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2025-11-03 16:50:53 +0200

    Create header and mobile menu

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ const less = require('gulp-less');
 function build() {
     return gulp.src('./less/*.less') // Only top-level LESS files in the less folder
         .pipe(less({ compress: true })) // Sets the output to be compressed
+        .on('error', function (err) {
+            console.error('LESS compilation error:', err.message);
+            this.emit('end');
+        })
         .pipe(gulp.dest('./styles')); // The output folder destination
 };
 
