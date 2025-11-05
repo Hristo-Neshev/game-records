2ace1c01b88977a38467916ed1406f6f850843aa
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2025-11-05 10:46:50 +0200

    clean up and add new case for toggleDialog

--- scripts/utils/toggleDialog.js
+++ scripts/utils/toggleDialog.js
@@ -3,10 +3,6 @@ const dialogTitleSpanEl = document.querySelector(".js-delete-dialog-title");
 export default function toggleDialogElement(action, dialogEl, triggerBtnEl) {
     const dialogType = dialogEl.dataset.type;
     const btnType = triggerBtnEl.dataset.type;
-    // switch (action) {
-    //     case "open": dialogEl.show(); break;
-    //     case "close": dialogEl.close(); break;
-    // }
 
     if (action === "close") {
         dialogEl.close();
@@ -25,11 +21,13 @@ export default function toggleDialogElement(action, dialogEl, triggerBtnEl) {
         dialogTitleSpanEl.textContent = gameName;
         dialogEl.dataset.deleteDataId = rowId;
         dialogEl.show();
+    } else if (dialogType === edit) {
+        const currentDataRowEl = triggerBtnEl.closest(".js-data-table-row");
+        const gameNameTdEl = currentDataRowEl.querySelector(".js-game-name-td");
+        const gameName = gameNameTdEl.textContent.trim();
+        const rowId = currentDataRowEl.id;
+        dialogTitleSpanEl.textContent = gameName;
+        dialogEl.dataset.deleteDataId = rowId;
+        dialogEl.show();
     }
 }
-
-function addDataDialogOpen() { }
-
-function deleteDataDialogOpen() { }
-
-function editDataDialogOpen() { }
